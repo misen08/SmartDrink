91ab3449aee43bc9658ed4a394f95af2e34e9b34
Leonardo Pignataro <leonardo.pignataro@outlook.com>
2018-08-21 15:18:05 -0300

    Leo - 2018-08-21 + Se agrega clase MyFirebaseMessagingService para el manejo de notificaciones. * Se modifica manifest * Se modifican los dos archivo 'build.gradle' para agregar dependencias de firebase messaging

---
+++ app/src/main/java/xyris/smartdrink/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,84 @@
+package xyris.smartdrink.fcm;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import xyris.smartdrink.PantallaInicial;
+import xyris.smartdrink.R;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final String TAG = "NOTIFICACIONES";
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.e(TAG, "Token: " + s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+        Log.d(TAG, "Mensaje recibido de: " + from);
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Notificación: " + remoteMessage.getNotification().getBody());
+
+            showNotificationMessage(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Data: " + remoteMessage.getData());
+        }
+    }
+
+
+    private void showNotificationMessage(String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.drink)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentInfo("Info");
+
+        notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+    }
+}
